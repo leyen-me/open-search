1a0d02abac2eee6a47e3bff0d52daab2029592ef
DIFFFFFFT <672228275@qq.com>
2023-11-08 09:12:07 +0800

    新增功能

--- src/data/me.ts
+++ src/data/me.ts
@@ -7,77 +7,108 @@ import FaviconSavefromImage from "@/assets/favicon/favicon_savefrom.png";
 import FaviconIiilabImage from "@/assets/favicon/favicon_iiilab.png";
 import FaviconGoogleTranslateImage from "@/assets/favicon/favicon_google_translate.png";
 import FaviconDifffffftImage from "@/assets/favicon/favicon_difffffft.png";
+import FaviconTinyPngImage from "@/assets/favicon/favicon_tinypng.png";
+import FaviconPdfToolsImage from "@/assets/favicon/favicon_pdf_tools.png";
+import FaviconDeeplImage from "@/assets/favicon/favicon_deepl.png";
+import FaviconCodeServerImage from "@/assets/favicon/favicon_codeserver.png";
 
 export default [
-  {
-    title: "常用工具",
-    list: [
-      {
-        title: "我的博客",
-        desc: "记录生活的瞬间",
-        icon: FaviconDifffffftImage,
-        link: "http://blog.difffffft.com/",
-      },
-      {
-        title: "Google翻译",
-        desc: "全世界最大，最专业的翻译工具",
-        icon: FaviconGoogleTranslateImage,
-        link: "https://translate.google.com/",
-      },
-      {
-        title: "有道翻译",
-        desc: "最专业的翻译工具",
-        icon: FaviconYouDaoYunTranslateImage,
-        link: "https://fanyi.youdao.com/",
-      },
-      {
-        title: "视频下载",
-        desc: "哔哩哔哩视频下载",
-        icon: FaviconIiilabImage,
-        link: "https://bilibili.iiilab.com/",
-      },
-      {
-        title: "savefrom",
-        desc: "Youtube视频高清下载",
-        icon: FaviconSavefromImage,
-        link: "https://zh.savefrom.net/226/",
-      },
-    ],
-  },
-  {
-    title: "电影电视",
-    list: [
-      {
-        title: "抖音",
-        desc: "记录美好生活",
-        icon: FaviconDouYinImage,
-        link: "https://www.douyin.com/",
-      },
-      {
-        title: "哔哩哔哩",
-        desc: "国内知名的视频弹幕网站",
-        icon: FaviconBilibiliImage,
-        link: "https://www.bilibili.com/",
-      },
-    ],
-  },
-  {
-    title: "人工智能",
-    list: [
-      {
-        title: "ChatGPT",
-        desc: "自然语言处理最强大的模型",
-        icon: FaviconChatgptImage,
-        link: "https://chat.openai.com/chat",
-        vpn: true,
-      },
-      {
-        title: "Midjourney",
-        desc: "AIGC图片领域最强大的模型",
-        icon: FaviconMidjourneyImage,
-        link: "https://www.midjourney.com/",
-        vpn: true,
-      },
-    ],
-  },
+    {
+        title: "常用工具",
+        list: [
+            {
+                title: "MyLineVscode",
+                desc: "我的在线编辑器",
+                icon: FaviconCodeServerImage,
+                link: "http://206.237.30.141:8443/?folder=/config/workspace",
+            },
+            {
+                title: "我的博客",
+                desc: "记录生活的瞬间",
+                icon: FaviconDifffffftImage,
+                link: "http://blog.difffffft.com/",
+            },
+            {
+                title: "Deepl翻译",
+                desc: "深度学习翻译",
+                icon: FaviconDeeplImage,
+                link: "https://www.deepl.com/translator",
+            },
+            {
+                title: "Google翻译",
+                desc: "全世界最大，最专业的翻译工具",
+                icon: FaviconGoogleTranslateImage,
+                link: "https://translate.google.com/",
+                vpn: true
+            },
+            {
+                title: "有道翻译",
+                desc: "最专业的翻译工具",
+                icon: FaviconYouDaoYunTranslateImage,
+                link: "https://fanyi.youdao.com/",
+            },
+            {
+                title: "视频下载",
+                desc: "哔哩哔哩视频下载",
+                icon: FaviconIiilabImage,
+                link: "https://bilibili.iiilab.com/",
+            },
+            {
+                title: "savefrom",
+                desc: "Youtube视频高清下载",
+                icon: FaviconSavefromImage,
+                link: "https://zh.savefrom.net/226/",
+            },
+            {
+                title: "TinyPng",
+                desc: "图片压缩",
+                icon: FaviconTinyPngImage,
+                link: "https://tinypng.com/",
+                vpn: true
+            },
+            {
+                title: "PdfTools",
+                desc: "PDF工具集",
+                icon: FaviconPdfToolsImage,
+                link: "https://tools.pdf24.org/zh/",
+                vpn: true
+            },
+        ],
+    },
+    {
+        title: "电影电视",
+        list: [
+            {
+                title: "抖音",
+                desc: "记录美好生活",
+                icon: FaviconDouYinImage,
+                link: "https://www.douyin.com/",
+            },
+            {
+                title: "哔哩哔哩",
+                desc: "国内知名的视频弹幕网站",
+                icon: FaviconBilibiliImage,
+                link: "https://www.bilibili.com/",
+            },
+        ],
+    },
+    {
+        title: "人工智能",
+        list: [
+            {
+                title: "ChatGPT",
+                desc: "自然语言处理最强大的模型",
+                icon: FaviconChatgptImage,
+                link: "https://chat.openai.com/chat",
+                vpn: true,
+            },
+            {
+                title: "Midjourney",
+                desc: "AIGC图片领域最强大的模型",
+                icon: FaviconMidjourneyImage,
+                link: "https://www.midjourney.com/",
+                vpn: true,
+            },
+        ],
+    },
 ];
--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,41 @@
-import { defineConfig, loadEnv } from "vite";
+import {defineConfig, loadEnv} from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 import Pages from "vite-plugin-pages";
 
-export default defineConfig(({ command, mode, ssrBuild }) => {
-  const env = loadEnv(mode, process.cwd());
-  const { VITE_APP_ENV } = env;
-  return {
-    base: VITE_APP_ENV === "production" ? "./" : "/",
-    plugins: [
-      vue(),
-      Pages({
-        dirs: "src/pages",
-        exclude: ["**/components/*.vue"],
-      }),
-    ],
-    resolve: {
-      alias: {
-        "~": path.resolve(__dirname, "./"),
-        "@": path.resolve(__dirname, "./src"),
-      },
-      extensions: [
-        ".mjs",
-        ".js",
-        ".ts",
-        ".jsx",
-        ".tsx",
-        ".json",
-        ".vue",
-        ".css",
-        ".scss",
-      ],
-    },
-    server: {
-      port: 81,
-      host: true,
-      open: true,
-    },
-  };
+export default defineConfig(({command, mode, ssrBuild}) => {
+    const env = loadEnv(mode, process.cwd());
+    const {VITE_APP_ENV} = env;
+    return {
+        base: VITE_APP_ENV === "production" ? "./" : "/",
+        plugins: [
+            vue(),
+            Pages({
+                dirs: "src/pages",
+                exclude: ["**/components/*.vue"],
+            }),
+        ],
+        resolve: {
+            alias: {
+                "~": path.resolve(__dirname, "./"),
+                "@": path.resolve(__dirname, "./src"),
+            },
+            extensions: [
+                ".mjs",
+                ".js",
+                ".ts",
+                ".jsx",
+                ".tsx",
+                ".json",
+                ".vue",
+                ".css",
+                ".scss",
+            ],
+        },
+        server: {
+            port: 3000,
+            host: "0.0.0.0",
+            open: true,
+        },
+    };
 });
